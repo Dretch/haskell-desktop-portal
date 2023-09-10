775dfac5b5f41fba7960feb991abd73f6a43cbca
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2023-09-10 11:52:32 +0100

    Fix formatting.

--- src/Desktop/Portal/Secret.hs
+++ src/Desktop/Portal/Secret.hs
@@ -8,12 +8,12 @@ import Desktop.Portal.Internal (Client, await, sendRequest)
 import Network.Socket (Family (..), SocketType (..), close, defaultProtocol, socketPair, withFdSocket)
 import Network.Socket.ByteString (recv)
 import System.Posix (Fd (..))
- 
+
 secretsInterface :: InterfaceName
 secretsInterface = "org.freedesktop.portal.Secret"
 
 -- | Retrieve the application-specific secret.
--- 
+--
 -- Currently works in Gnome, but not KDE (see https://bugs.kde.org/show_bug.cgi?id=466197 ).
 -- The token parameter that is documented in the portal API specs is not supported either, as
 -- it is not clear exactly how this should work and it does not seem to be supported by Gnome.
